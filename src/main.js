ce899f51d64bbe4201f42717c633b6c56e904d6c
e <172247097@qq.com>
2017-09-02 08:24:54 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import EventEmitter from './mixins/event_emitter';
 import DCUI from './packages';
 import './assets/font-awesome/css/font-awesome.min.css';
+import './styles/base.scss';
 
 Vue.config.productionTip = false
 Vue.mixin(EventEmitter);
