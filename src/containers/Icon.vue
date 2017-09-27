4df836f2585b37d04d8cf42dc43300f6e6275bfb
w-rudolph <172247097@qq.com>
2017-09-27 17:22:02 +0800

    update

--- src/containers/Icon.vue
+++ src/containers/Icon.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container">
     <h3>Icon 组件</h3>
+    <hr> reference: <a href="http://ionicons.com/" target="blank">http://ionicons.com/</a><br>
+    <d-icon name="load-a"></d-icon>
+    <d-icon name="load-b"></d-icon>
+    <d-icon name="load-c"></d-icon>
+    <d-icon name="arrow-down-b"></d-icon>
   </div>
 </template>
 
 <script>
 export default {
- 
+
 }
 </script>
