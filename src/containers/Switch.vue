082ab1d5bc30ac4899c325ad17bd7e49de26efb1
e <172247097@qq.com>
2017-10-24 23:49:24 +0800

    update

---
+++ src/containers/Switch.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="padding: 0 20px">
+        <h2>Switch 组件</h2>
+        <hr>
+        <d-switch></d-switch>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
