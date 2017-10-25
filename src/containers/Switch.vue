020ba532085f41f88d56f25c1c0fc2246b94ea9b
w-rudolph <172247097@qq.com>
2017-10-25 13:44:47 +0800

    update

--- src/containers/Switch.vue
+++ src/containers/Switch.vue
@@ -2,12 +2,41 @@
     <div style="padding: 0 20px">
         <h2>Switch 组件</h2>
         <hr>
-        <d-switch></d-switch>
+        <h4>Basic</h4>
+        <d-switch v-model="value">
+            <span slot="off">关</span>
+            <span slot="on">开</span>
+        </d-switch>
+        <h4>Disabled</h4>
+        <d-switch disabled :value="true">
+            <span slot="off">关</span>
+            <span slot="on">开</span>
+        </d-switch>
+        <h4>Size</h4>
+        <h5>large</h5>
+        <d-switch size="large" :value="true">
+            <span slot="off">关</span>
+            <span slot="on">开</span>
+        </d-switch>
+        <h5>small</h5>
+        <d-switch size="small" :value="true">
+            <span slot="off">关</span>
+            <span slot="on">开</span>
+        </d-switch>
+        <h4>Custom width</h4>
+        <d-switch :value="false" style="width: 100px">
+            <span slot="off">状态:关闭</span>
+            <span slot="on">状态:开启</span>
+        </d-switch>
     </div>
 </template>
 <script>
 export default {
-
+    data() {
+        return {
+            value: true
+        }
+    }
 }
 </script>
 
--- src/packages/switch/switch.vue
+++ src/packages/switch/switch.vue
@@ -1,10 +1,62 @@
 <template>
-    <div class="d-switch">
-        To be done!
+    <div :class="['d-switch', size ? 'd-switch--'+size : '',currentValue ? 'd-switch--checked' : '', disabled ? 'disabled' : '']" @click="handleClick">
+        <div class="d-switch__inner">
+            <slot name="on" v-if="currentValue"></slot>
+            <slot name="off" v-else></slot>
+        </div>
+        <div class="d-switch__dot" ref="dot" :style="dotStyle"></div>
     </div>
 </template>
 <script>
 export default {
-    name: 'DSwitch'
+    name: 'DSwitch',
+    props: {
+        value: {
+            type: Boolean,
+            default: false
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        size: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            currentValue: this.value,
+            dotStyle: {
+                left: '2px'
+            }
+        }
+    },
+    watch: {
+        value(val) {
+            this.currentValue = val;
+            this.$emit('change', val);
+            this.setDotStyle();
+        }
+    },
+    methods: {
+        handleClick() {
+            if (this.disabled) {
+                return;
+            }
+            this.currentValue = !this.currentValue;
+            this.setDotStyle();
+            this.$emit('input', this.currentValue);
+        },
+        setDotStyle() {
+            const offset = this.$el.offsetWidth - this.$refs.dot.offsetWidth - 4;
+            this.dotStyle = {
+                left: this.currentValue ? offset + 'px' : '2px'
+            };
+        }
+    },
+    mounted() {
+        this.setDotStyle();
+    }
 }
 </script>
