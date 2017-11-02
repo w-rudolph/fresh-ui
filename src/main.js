1268681112d01f069e1e6caddc8d97573decb69a
w-rudolph <172247097@qq.com>
2017-11-02 18:07:24 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import FreshUI from './packages';
-import './assets/ionicons/ionicons.scss';
 import './packages/sass/index.scss';
 
 Vue.config.productionTip = false
 Vue.use(FreshUI);
 
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  render: h => h(App)
+    el: '#app',
+    router,
+    components: { App },
+    render: h => h(App)
 })
