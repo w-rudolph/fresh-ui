d0e18c6cb20ca3be87a2a339c4ec0573016f4a37
w-rudolph <172247097@qq.com>
2017-11-03 10:10:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import App from './App'
 import router from './router'
 import FreshUI from './packages';
 import './packages/sass/index.scss';
-import fui from 'fresh-ui';
+
 Vue.config.productionTip = false
 Vue.use(FreshUI);
-console.log(fui)
 
 new Vue({
     el: '#app',
